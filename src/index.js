f439a27fb3e1b6688581818cfbd27530009aea42
Karabo-Khoza <laurah.karabo@gmail.com>
2025-08-08 14:51:07 +0200

    Added current location

--- src/index.js
+++ src/index.js
@@ -61,6 +61,9 @@ setInterval(function () {
 //Event Listener for City Selection
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
